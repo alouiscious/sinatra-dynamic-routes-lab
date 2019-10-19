83110363b40e0506f3a95d5d67d7087cb2e26787
Alouiscious Media <info@reneesoul.com>
2019-10-19 03:01:30 -0700

    Done.

--- app.rb
+++ app.rb
@@ -12,22 +12,34 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @times_phrase = ''
-    @times_phrase = params[:number].to_i.times do 
-      "#{params[:phrase]}," 
+    times_phrase = ''
+    params[:number].to_i.times do 
+      times_phrase += params[:phrase]
     end
-    "@times_phrase"
+    times_phrase
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:words4]} #{params[:words5]}."
-    
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
-    add_num.to_s = "params[:number1].to_i + params[:number2].to_i"
-    sub_num.to_s = "params[:number2].to_i - params[:number1].to_i"
-    mlt_num.to_s = "params[:number1].to_i * params[:number2].to_i"
-    div_num.to_s = "params[:number1].to_i / params[:number2].to_i"
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    
+    add_num = num1 + num2
+    sub_num = num1 - num2
+    mlt_num = num1 * num2
+    div_num = num1 / num2
+
+    if params[:operation] == 'add'
+      add_num.to_s 
+    elsif params[:operation] == 'subtract'
+      sub_num.to_s
+    elsif params[:operation] == 'multiply'
+      mlt_num.to_s
+    else params[:operation] == 'divide'
+      div_num.to_s
+    end
   end
 end
